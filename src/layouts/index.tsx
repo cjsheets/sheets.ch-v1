e2302f5b8b1b77ccfdbcafb11674b5afba3b853b
Chad Sheets <cjsheets@gmail.com>
2018-01-23 07:04:15 -0800

    fix mobile sidebar

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,6 +1,7 @@
 import 'prismjs/themes/prism-okaidia.css';
 import * as React from 'react';
 import { connect } from 'react-redux';
+import Sidebar from 'react-sidebar';
 
 import MobileSidebar from '../components/mobile-sidebar/mobile-sidebar';
 import SiteFooter from '../components/site-footer/site-footer';
@@ -9,7 +10,6 @@ import { IStoreState, primaryNavigation, toggleSidebar } from '../store';
 
 import '../styles/normalize.css';
 import * as styles from '../styles/styles.scss';
-
 
 interface IDefaultLayoutProps extends React.HTMLProps<HTMLDivElement> {
   location: {
@@ -22,15 +22,22 @@ interface IDefaultLayoutProps extends React.HTMLProps<HTMLDivElement> {
 export const DefaultLayout = ({ location, children, visible }: IDefaultLayoutProps) => {
   const { pathname } = location;
 
+  const renderMobileSidebar = () => {
+    return (<MobileSidebar pathname={pathname} items={primaryNavigation} />);
+  };
+
   return (
-    <div className={styles.siteBody}>
+    <Sidebar
+      open={visible}
+      rootClassName={styles.siteBody}
+      sidebar={renderMobileSidebar()}
+    >
       <SiteHeader pathname={pathname} items={primaryNavigation} />
-      <MobileSidebar items={primaryNavigation} />
       <div className={styles.pageBody}>
         {children()}
       </div>
       <SiteFooter />
-    </div>
+    </Sidebar>
   );
 };
 
