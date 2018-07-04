d673de3c335b85290e3d59c38691b5ddb6faf781
Chad Sheets <chad@sheets.ch>
2018-07-04 12:26:30 -0700

    fix path matching

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,72 +1,124 @@
 const _ = require('lodash');
-const Promise = require('bluebird');
 const path = require('path');
 const { createFilePath } = require('gatsby-source-filesystem');
 
-exports.createPages = ({ graphql, actions }) => {
+/*
+ *  Implement the Gatsby API `createPages`.
+ *  This is called after the Gatsby bootstrap is finished
+ *  so you have access to any information necessary to
+ *  programatically create pages.
+ */
+exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions;
+  const blogPost = path.resolve('./src/templates/blog-post.tsx');
 
-  return new Promise((resolve, reject) => {
-    const blogPost = path.resolve('./src/templates/blog-post.tsx');
-    resolve(
-      graphql(
-        `
-          {
-            allMarkdownRemark(
-              sort: { fields: [frontmatter___date], order: DESC }
-              limit: 1000
-            ) {
-              edges {
-                node {
-                  fields {
-                    slug
-                  }
-                  frontmatter {
-                    title
-                  }
-                }
+  const allMarkdown = await graphql(
+    `
+      {
+        allMarkdownRemark(
+          sort: { fields: [frontmatter___date], order: DESC }
+          limit: 1000
+        ) {
+          edges {
+            node {
+              fields {
+                slug
+              }
+              frontmatter {
+                title
               }
             }
           }
-        `,
-      ).then(result => {
-        if (result.errors) {
-          console.log(result.errors);
-          reject(result.errors);
         }
+      }
+    `,
+  );
 
-        // Create blog posts pages.
-        const posts = result.data.allMarkdownRemark.edges;
+  if (allMarkdown.errors) {
+    console.log(allMarkdown.errors);
+    throw new Error(allMarkdown.errors);
+  }
 
-        _.each(posts, (post, index) => {
-          const previous =
-            index === posts.length - 1 ? null : posts[index + 1].node;
-          const next = index === 0 ? null : posts[index - 1].node;
+  // Create blog posts pages.
+  const posts = allMarkdown.data.allMarkdownRemark.edges;
 
-          createPage({
-            path: post.node.fields.slug,
-            component: blogPost,
-            context: {
-              slug: post.node.fields.slug,
-              previous,
-              next,
-            },
-          });
-        });
-      }),
-    );
+  _.each(posts, (post, index) => {
+    const previous =
+      index === posts.length - 1 ? null : posts[index + 1].node;
+    const next = index === 0 ? null : posts[index - 1].node;
+
+    createPage({
+      path: post.node.fields.slug,
+      component: blogPost,
+      context: {
+        slug: post.node.fields.slug,
+        previous,
+        next,
+      },
+    });
   });
+
+  // // Create tag pages
+  // const tagTemplate = path.resolve(__dirname, '../src/templates/tags.tsx');
+  // posts
+  //   .reduce((mem, post) =>
+  //     cleanArray(mem.concat(get(post, 'frontmatter.tags')))
+  //   , [])
+  //   .forEach(tag => {
+  //     createPage({
+  //       path: `/post/tags/${tag}/`,
+  //       component: tagTemplate,
+  //       context: {
+  //         tag
+  //       }
+  //     });
+  //   });
+
+  // // Create post pagination
+  // const pageCount = Math.ceil(posts.length / POSTS_PER_PAGE);
+  // times(pageCount, index => {
+  //   createPage({
+  //     path: `/post/page/${index + 1}/`,
+  //     component: slash(templates.page),
+  //     context: {
+  //       skip: index * POSTS_PER_PAGE
+  //     }
+  //   });
+  // });
 };
 
 exports.onCreateNode = ({ node, actions, getNode }) => {
+  const FILENAME_REGEX = /([^/]+)\.md$/;
+  const BLOG_POST_FILENAME_REGEX = /([0-9]+)\-([0-9]+)\-([0-9]+)\-(.+)\.md$/;
   const { createNodeField } = actions;
 
   if (node.internal.type === `MarkdownRemark`) {
+    const {permalink} = node.frontmatter;
+    const {relativePath} = getNode(node.parent);
     const value = createFilePath({ node, getNode });
-    createNodeField({
-      name: `slug`,
-      node,
-      value,
-    });
+
+    let slug = permalink;
+
+    if (!slug) {
+      const pathRoot = relativePath.includes('post') && 'post' || relativePath.includes('project') && 'project';
+      if (pathRoot) {
+        const match = BLOG_POST_FILENAME_REGEX.exec(relativePath);
+        const year = match[1];
+        const month = match[2];
+        const day = match[3];
+        const filename = match[4];
+
+        slug = `/${pathRoot}/${year}/${month}/${day}/${filename}`;
+
+        const date = new Date(year, month - 1, day);
+
+        createNodeField({node, name: 'date', value: date.toJSON()});
+      } else {
+        const match = FILENAME_REGEX.exec(relativePath);
+        const filename = match[1];
+        slug = `/${filename}`
+      }
+    }
+    createNodeField({node, name: 'slug', value: slug});
   }
 };
