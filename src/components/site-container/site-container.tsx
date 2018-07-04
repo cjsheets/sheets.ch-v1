a10b044a5953e12f005979baa849b6713d587c6d
Chad Sheets <chad@sheets.ch>
2018-07-04 10:28:06 -0700

    responsive sizing

--- src/components/site-container/site-container.tsx
+++ src/components/site-container/site-container.tsx
@@ -12,19 +12,22 @@ interface ISiteContainer {
 import '../../styles/normalize.css';
 import '../../styles/prism-vs.css';
 import '../../styles/theme.scss';
+import * as styles from './site-container.module.scss';
 
 class SiteContainer extends React.Component<ISiteContainer, {}> {
   render() {
     const { location, children } = this.props;
 
     return (
-      <div>
+      <div className={styles.sitePadding}>
         <SiteHeader title={'frontmatter.title'} />
           <Helmet>
             <title>{this.props.pageTitle}</title>
             <link href='https://fonts.googleapis.com/css?family=Lato:300,400|Open+Sans:400,700' rel='stylesheet' />
           </Helmet>
-          {children}
+          <div className={styles.contentContainer}>
+            {children}
+          </div>
         <SiteFooter />
       </div>
     );
