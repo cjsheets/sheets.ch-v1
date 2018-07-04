18ad7f59f39a437b8f35676dcb2181657e44d36b
Chad Sheets <chad@sheets.ch>
2018-07-04 16:52:54 -0700

    add about

---
+++ src/pages/ad.tsx
@@ -0,0 +1,33 @@
+import { graphql } from 'gatsby'
+import get from 'lodash/get';
+import React from 'react';
+
+import { MarkdownRemarkConnection } from '../../@types/graphql-types';
+import SiteContainer from '../components/site-container/site-container';
+
+interface IAbout {
+  data: { allMarkdownRemark: MarkdownRemarkConnection; };
+  location: any;
+}
+
+const About = (props: IAbout) =>(
+  <SiteContainer location={props.location}>
+    <div dangerouslySetInnerHTML={{__html: get(props, 'data.allMarkdownRemark.edges[0].node.html')}} />
+  </SiteContainer>)
+
+export default About;
+
+export const pageQuery = graphql`
+  query AboutMarkdown {
+    allMarkdownRemark(filter: {fields: {slug: {eq: "/about"}}}) {
+      edges {
+        node {
+          frontmatter {
+            title
+          }
+          html
+        }
+      }
+    }
+  }
+`;
